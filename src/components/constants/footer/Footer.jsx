c4083b8c1ab28fd8756f4af586cdc2563f024ff0
Aaron Shames <aaron@1coin.io>
2023-02-10 15:24:11 -0500

    Update Footer.jsx

--- src/components/constants/footer/Footer.jsx
+++ src/components/constants/footer/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
                     </li>
                    */}
                     <li className=" btn btn-lg btn-BuysBomb">
-                        <a href="https://www.bombswap.xyz/swap">Buy sBomb <i className="fa-solid fa-arrow-right ms-1"></i></a>
+                        <a href="https://www.bombswap.xyz/swap?inputCurrency=ETH&outputCurrency=0x2a4a10237CBec8ef5FFe4eE4770D467966c4FEc4">Buy sBomb <i className="fa-solid fa-arrow-right ms-1"></i></a>
                     </li>
                 </ul>
             </div>
